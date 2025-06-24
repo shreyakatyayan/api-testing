4c4bb2114bc6e930f4136b408ad1e05c05aa9b9c
Shreya Katyayan <139438143+shreyakatyayan@users.noreply.github.com>
2025-06-24 09:39:39 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const { app, connectDB } = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
